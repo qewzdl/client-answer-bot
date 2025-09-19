85460eaab38dd9c75b8cc9d586e699b0c6c5a735
Qewzdl Despayer <qewzdl@gmail.com>
2025-09-19 22:16:40 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -71,6 +71,7 @@ def init_driver():
     
     # Улучшенные опции для стабильности
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--disable-extensions")
